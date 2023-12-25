8a6f21d2213c3e32b1299a5c9b559039338dbb17
Vincent Wang <ws_dev@163.com>
2023-12-25 22:33:57 +0800

    support debian 12

---
+++ debian/12/install.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#
+# This script is for Debian 12 to enable Hyper-V Enhanced Session
+#
+
+###############################################################################
+# Use HWE kernel packages
+#
+HWE=""
+
+###############################################################################
+# Update our machine to the latest code if we need to.
+#
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo 'This script must be run with root privileges' >&2
+    exit 1
+fi
+
+apt update && apt upgrade -y
+
+if [ -f /var/run/reboot-required ]; then
+    echo "A reboot is required in order to proceed with the install." >&2
+    echo "Please reboot and re-run this script to finish the install." >&2
+    exit 1
+fi
+
+###############################################################################
+# XRDP
+#
+
+# Install the xrdp service so we have the auto start behavior
+apt install -y xrdp
+
+systemctl stop xrdp
+systemctl stop xrdp-sesman
+
+# Configure the installed XRDP ini files.
+# use vsock transport.
+sed -i_orig -e 's/port=3389/port=vsock:\/\/-1:3389/g' /etc/xrdp/xrdp.ini
+# use rdp security.
+sed -i_orig -e 's/security_layer=negotiate/security_layer=rdp/g' /etc/xrdp/xrdp.ini
+# remove encryption validation.
+sed -i_orig -e 's/crypt_level=high/crypt_level=none/g' /etc/xrdp/xrdp.ini
+# disable bitmap compression since its local its much faster
+sed -i_orig -e 's/bitmap_compression=true/bitmap_compression=false/g' /etc/xrdp/xrdp.ini
+
+# Changed the allowed_users
+sed -i_orig -e 's/allowed_users=console/allowed_users=anybody/g' /etc/X11/Xwrapper.config
+
+# Blacklist the vmw module
+if [ ! -e /etc/modprobe.d/blacklist-vmw_vsock_vmci_transport.conf ]; then
+  echo "blacklist vmw_vsock_vmci_transport" > /etc/modprobe.d/blacklist-vmw_vsock_vmci_transport.conf
+fi
+
+#Ensure hv_sock gets loaded
+if [ ! -e /etc/modules-load.d/hv_sock.conf ]; then
+  echo "hv_sock" > /etc/modules-load.d/hv_sock.conf
+fi
+
+
+# reconfigure the service
+systemctl daemon-reload
+systemctl start xrdp
+
+#
+# End XRDP
+###############################################################################
+
+echo "Install is complete."
+echo "Reboot your machine to begin using XRDP."
